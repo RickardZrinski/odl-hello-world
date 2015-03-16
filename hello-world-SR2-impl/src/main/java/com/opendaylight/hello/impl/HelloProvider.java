dafd6b1fa7035052d3d646cb126959685c8d0347
Rickard Zrinski <ricco3@hotmail.com>
2015-03-16 19:05:50 +0100

    Fix HelloProvider

--- hello-world-SR2-impl/src/main/java/com/opendaylight/hello/impl/HelloProvider.java
+++ hello-world-SR2-impl/src/main/java/com/opendaylight/hello/impl/HelloProvider.java
@@ -4,24 +4,20 @@ import java.util.Collection;
 import java.util.Collections;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ConsumerContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
-import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareProvider;
 import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.world.rev150105.HelloWorldService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-
-public class HelloProvider implements BindingAwareProvider, BundleActivator, AutoCloseable {
+public class HelloProvider extends AbstractBindingAwareProvider implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloProvider.class);
     private RpcRegistration<HelloWorldService> helloService;
 
     public HelloProvider() {
-        LOG.info("HelloProvider Constructor Called");
+        LOG.info("HelloProvider Constructor Called!");
     }
 
     @Override
@@ -36,25 +32,11 @@ public class HelloProvider implements BindingAwareProvider, BundleActivator, Aut
         helloService.close();
     }
 
-    @Override
-    public void start(BundleContext context) {
-        LOG.info("HelloProvider Bundle Start");
-    }
-
-    @Override
-    public void stop(BundleContext context) {
-        LOG.info("HelloProvider Bundle Stop");
-    }
-
     public Collection<? extends RpcService> getImplementations() {
         return Collections.emptySet();
     }
 
     public Collection<? extends ProviderFunctionality> getFunctionality() {
         return Collections.emptySet();
-    }
-
-    public void onSessionInitialized(ConsumerContext session) {
-        // Deprecated
     }
 }
